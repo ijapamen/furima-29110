2f1d44c67155e8eb58390a5551f5d1bf93fe1be9
ijapamen <ijapamen@icloud.com>
2020-12-04 15:02:32 +0900

    バリデーションの正規表現を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
     validates :nickname
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates :password, format: { with: PASSWORD_REGEX }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
