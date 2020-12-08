02efeaec93dd92b600690f297e7e7ef4b9e75c85
ijapamen <ijapamen@icloud.com>
2020-12-08 13:57:48 +0900

    Categoryモデルの作成とアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  # with_options presence: true do
+  #   validates :name
+  #   validates :details
+
+  #   with_options numericality: { other_than: 0 } do
+  #     validates :category_id
+  #     validates :condition_id
+  #     validates :delivery_fee_id
+  #     validates :prefecture_id
+  #     validates :days_id
+  #   end
+
+  #   validates :user
+  # end  
+
 end
