0a8cc9be189c7c15d29bec88c033414cedb29094
ijapamen <ijapamen@icloud.com>
2020-12-08 15:09:29 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,19 +5,21 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :delivery_fee, :prefecture, :day
 
-  # with_options presence: true do
-  #   validates :name
-  #   validates :details
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :details
 
-  #   with_options numericality: { other_than: 0 } do
-  #     validates :category_id
-  #     validates :condition_id
-  #     validates :delivery_fee_id
-  #     validates :prefecture_id
-  #     validates :days_id
-  #   end
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :days_id
+    end
 
-  #   validates :user
-  # end  
+    validates_inclusion_of :price, in:300..9999999
+    validates :user
+  end  
 
 end
