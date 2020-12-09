7b54b602e950dcc8a6c6063c52da73b3ca80e948
ijapamen <ijapamen@icloud.com>
2020-12-09 15:05:42 +0900

    出品した商品をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.new
   end
 
   def new
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    if @item.valid?
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user_id)
+    params.require(:item).permit(:image, :name, :details, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_id, :price).merge(user_id: current_user.id)
   end
 end
