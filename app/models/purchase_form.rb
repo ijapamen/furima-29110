8cd9d4712d659800475f62146e74f92061b9619a
ijapamen <ijapamen@icloud.com>
2020-12-18 12:57:22 +0900

    バリデーションの処理

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,20 +1,19 @@
 class PurchaseForm
   include ActiveModel::Model
 
-  attr_accessor :item, :user, :postal_code, :prefecture, :city, :address, :building, :phone_number, :purchase_record
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :order_id
   # ここにバリデーションの処理を書く
   with_options presence: true do
-    validates :item
-    validates :user
-    validates :postal_code
-    validates :prefecture_id
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
-    validates :address
-    validates :building
-    validates :phone_number
-    validates :purchase_record
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :order_id
   end
-
+  validates :building_name
 
   def save
     # 各テーブルにデータを保存する処理を書く
