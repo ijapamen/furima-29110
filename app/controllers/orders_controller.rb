d5ad90f7bb85271691f547e864f022062dcb8720
ijapamen <ijapamen@icloud.com>
2020-12-19 14:53:05 +0900

    フォームの情報を保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+
+  def index
+    @purchase_form = PurchaseForm.new
+  end
+
+  def create
+    @purchase_form = PurchaseForm.new(purchase_params)
+    if @purchase_form.valid?
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_form).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
