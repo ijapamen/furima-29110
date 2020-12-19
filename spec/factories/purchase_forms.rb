ce20900b7aa5c2a90ac1caa4fa62809f6f7cb78f
ijapamen <ijapamen@icloud.com>
2020-12-19 15:17:13 +0900

    値をFactoryBotで指定

--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :purchase_form do
     
+    item_id { 1 }
+    user_id { 1 }
+    postal_code { '123-4567' }
+    prefecture_id { 1 }
+    city { '大阪市' }
+    house_number { '1-1' }
+    phone_number { '08012345678' }
+    
   end
 end
