fda43cc0a7b12ebcfca37c05b3f8c8f3f9460860
ijapamen <ijapamen@icloud.com>
2020-12-22 14:48:39 +0900

    URLから直接、購入済み商品の購入ページへ遷移しようとするとトップに遷移させる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    if item.valid?
-      item.save
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
       redirect_to root_path
     else
       render :new
